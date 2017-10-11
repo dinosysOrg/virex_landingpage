e3c9974a8fd6809ce7134f332090ee34a9f0d216
Hiep Nguyen <ngochiep9523@gmail.com>
2017-10-11 17:52:29 +0700

    implement js

--- src/js/scripts.project.js
+++ src/js/scripts.project.js
@@ -20,20 +20,38 @@ $("body").niceScroll({
   zindex: 999
 });
 
+photos = [ {href : 'https://via.placeholder.com/960x480', title : 'Title'}, {href : 'https://via.placeholder.com/960x480', title : 'Title'}, {href : 'https://via.placeholder.com/960x480', title : 'Title'}, {href : 'https://via.placeholder.com/960x480', title : 'Title'}, {href : 'https://via.placeholder.com/960x480', title : 'Title'} ]
+
 function activeTab(event, id){
-  // var i, tabcontent, tablinks, itemId;
-  // tabcontent = $('.section-proj-6__tab-content_item');
-  // for (i = 0; i < tabcontent.length; i++) {
-  //     tabcontent[i].style.display = "none";
-  // }
-  // tablinks = $('.tablinks');
-  // for (i = 0; i < tablinks.length; j++) {
-  //     tablinks[i].className = tablinks[i].className.replace(" active", "");
-  // }
-  // itemId = '#item' + id;
-  // $(itemId).style.display = "block";
-  // evt.currentTarget.className += " active";
-  // alert('aaa');
-  console.log(event);
-  console.log(id);
+  var i, tabcontent, tablinks, itemId, prevActive;
+  tabcontent = $('.section-proj-6__tab-content_item');
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+  $('.active').removeClass('active');
+  itemId = '#item' + id;
+  $(itemId).css('display', 'block');
+  event.target.classList.add('active');
 }
+
+$('#defaultOpen').click();
+function openGallery(photos) {
+  var data = JSON.parse(photos)
+  $.fancybox.open(data,{
+      thumbs : {
+        autoStart : true
+      },
+      scrollOutside: true,
+    });
+}
+
+$('.more').click(function(){
+  var moreBlock = $('.section-proj-4__detail__content__more');
+  if (moreBlock.css('display') === 'none') {
+    $(this).html('ẩn nội dung');
+    moreBlock.css('display', 'inline');
+  } else {
+    $(this).html('xem thêm');
+    moreBlock.css('display', 'none');
+  }
+});
